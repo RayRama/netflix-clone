21946f61970fef0f424a41f4d94cad1375e3d8c7
RayRama <rayramadita12@gmail.com>
2023-04-06 15:47:08 +0700

    update

--- src/models/Media.tsx
+++ src/models/Media.tsx
@@ -39,88 +39,9 @@ export abstract class Media implements MediaInterface {
     if (this.premiumOnly && (!user || !user.checkPremium())) {
       return false;
     }
-
     return true;
   }
 
   abstract getMovieDetails(): void;
   abstract getMovieDescription(): void;
 }
-
-// export abstract class Movie extends Media {
-//   // inheritance
-//   constructor(
-//     title: string,
-//     description: string,
-//     rating: number,
-//     genre: string,
-//     image?: string,
-//     premiumOnly?: boolean
-//   ) {
-//     super(title, description, rating, genre, image, premiumOnly);
-//   }
-
-//   static createMovie(data: MediaInterface[]): Movie[] {
-//     const movies = data.map(
-//       (movieData: MediaInterface) =>
-//         new Movie(
-//           movieData.title,
-//           movieData.description,
-//           movieData.rating,
-//           movieData.genre,
-//           movieData.image,
-//           movieData.premiumOnly
-//         )
-//     );
-
-//     return movies;
-//   }
-
-//   getMovieDescription(): void {
-//     alert(this.description);
-//   }
-
-//   getMovieDetails(): void {
-//     alert(
-//       `Title: ${this.title} | Rating: ${this.rating} | Genre: ${this.genre}`
-//     );
-//   }
-
-//   // getMovieDetails() and getMovieDescription() is abstracted from Media class
-// }
-
-// export class Series extends Media {
-//   // inheritance
-//   constructor(
-//     title: string,
-//     description: string,
-//     rating: number,
-//     genre: string,
-//     image?: string,
-//     public type: string
-//   ) {
-//     super(title, description, rating, genre, image);
-//   }
-
-//   static createSeries(data: MediaInterface[]): Series[] {
-//     const series = data.map(
-//       (seriesData: MediaInterface) =>
-//         new Series(
-//           seriesData.title,
-//           seriesData.description,
-//           seriesData.rating,
-//           seriesData.genre,
-//           seriesData.image,
-//           seriesData.premiumOnly
-//         )
-//     );
-
-//     return series;
-//   }
-
-//   // polymorphism
-//   getMovieDescription(): void {
-//     Alert.alert("Ini deskripsi", this.description);
-//   }
-//   // getMovieDetails() and getMovieDescription() is abstracted from Movie class
-// }
